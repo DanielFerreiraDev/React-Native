bc961c4f8a35631a94e3e01affff8d8ac1bd48d4
Daniel Ferreira <ferreira-estudante@hotmail.com>
2020-03-02 14:44:33 -0300

    Adicionando Botao para addTarefa na Task

--- tasks/src/screens/AddTask.js
+++ tasks/src/screens/AddTask.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
-import { Modal, View, Text, StyleSheet, TouchableWithoutFeedback} from 'react-native'
+import { 
+    Modal, 
+    View, 
+    Text, 
+    StyleSheet, 
+    TouchableWithoutFeedback,
+    TouchableOpacity,
+    TextInput,
+} from 'react-native'
 
 import commonStyles from '../commonStyles'
 
+const initialState = { desc: ''}
 
 export default class AddTask extends Component {
+
+    state = {
+        ...initialState
+    }
 
     render(){
         return(
@@ -17,6 +30,19 @@ export default class AddTask extends Component {
                 </TouchableWithoutFeedback>
                 <View style={styles.container}>
                     <Text style={styles.header}>Nova tarefa</Text>
+                    <TextInput style={styles.input}
+                        placeholder="Informe a Descrição..."
+                        onChangeText={desc => this.setState({ desc })}
+                        value={this.state.desc}/>
+                    <View style={styles.buttons}>
+                        <TouchableOpacity onPress={this.props.onCancel}>
+                            <Text style={styles.button}>Cancelar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text style={styles.button}>Salvar</Text>
+                        </TouchableOpacity>
+
+                    </View>
                 </View>
                 <TouchableWithoutFeedback
                     onPress={this.props.onCancel}>
@@ -33,7 +59,6 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0,0,0, 0.7)'
     },
     container: {
-        flex: 1,
         backgroundColor: '#FFF'
     },
     header: {
@@ -43,6 +68,25 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         padding: 15,
         fontSize: 18,
+    },
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        height: 40,
+        margin: 15,
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#E3E3E3',
+        borderRadius: 6,
+
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    button: {
+        margin: 20,
+        marginRight: 30,
+        color: commonStyles.colors.today
     }
 })
 
