c92d26310c86e337b7f1f7f05fce4ae71311123a
Daniel Ferreira <ferreira-estudante@hotmail.com>
2020-03-02 14:23:31 -0300

    Atualizando os componentes para dar uma formatação na tela de AddTarefa

--- tasks/src/screens/AddTask.js
+++ tasks/src/screens/AddTask.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { Modal, View, StyleSheet, TouchableWithoutFeedback} from 'react-native'
+import { Modal, View, Text, StyleSheet, TouchableWithoutFeedback} from 'react-native'
+
+import commonStyles from '../commonStyles'
 
 
 export default class AddTask extends Component {
@@ -9,13 +11,17 @@ export default class AddTask extends Component {
             <Modal transparent={true} visible={this.props.isVisible}
                 onRequestClose={this.props.onCancel}
                 animationType= 'slide'>
-                 <TouchableWithoutFeedback
+                <TouchableWithoutFeedback
                     onPress={this.props.onCancel}>
-                     <View style={styles.background}>
-
-                     </View>
-                 </TouchableWithoutFeedback>
-
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova tarefa</Text>
+                </View>
+                <TouchableWithoutFeedback
+                    onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
             </Modal>
         )
     }
@@ -25,6 +31,18 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: 'rgba(0,0,0, 0.7)'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF'
+    },
+    header: {
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 18,
     }
 })
 
